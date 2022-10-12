6c44433e0100f6518de381951fd7901bf6d1b58f
sumit-27-07 <sumitkumarsaw1994@gmail.com>
2022-10-12 14:13:02 +0530

    edit code

--- Sorting/bubble_sort.c
+++ Sorting/bubble_sort.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 int main(){
-    int arr[5]={5,4,3,2,1};
-    printf("%d",arr[0]);
-    return 0;
+    int n,arr[20],i,j,a,swap,p=0;
+    printf("How many numbers wants in array:");
+    scanf("%d",&n);
+    printf("Enter %d numbers of array:",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=n-1;i>=0;i--){
+        swap=0;
+        for(j=0;j<=i;j++){
+            if(arr[j]>arr[j+1]){
+                a=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=a;
+                swap++;
+            }
+        }
+        if(swap==0){
+            break;
+        }
+        p++;
+    }
+    printf("Sorted array:");
+    for(i=0;i<n;i++){
+        printf("%d  ",arr[i]);
+    }
+    printf("\nNumber of passes:%d",p);
 }
